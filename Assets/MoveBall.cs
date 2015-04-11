880b6ef962a16ef85c039732a4a6b7069179e9cf
beepbeepdaddad <emmetdoyle@ymail.com>
2015-04-11 19:13:44 +0100

    ball can move left and right

---
+++ Assets/MoveBall.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBall : MonoBehaviour {
+
+	private Rigidbody2D ballRigidbody;
+	private SpriteRenderer render;
+
+	private float speed;
+	public float r, g, b;
+	
+
+	// Use this for initialization
+	void Start () {
+
+		ballRigidbody = this.rigidbody2D;
+		render = gameObject.GetComponent<SpriteRenderer> ();
+		r = 0f;
+		g = 0f;
+		b = 0f;
+		render.color = new Color (r, g, b, 1);
+
+		speed = 5.0f;
+
+		//print(GameObject.Find("MainCamera").renderer.bounds.size.x);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move = Input.GetAxis ("Horizontal");
+
+		ballRigidbody.velocity = new Vector2 (move * speed, ballRigidbody.velocity.y);
+	}
+}
