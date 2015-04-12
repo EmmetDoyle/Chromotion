3bceaea250802fa73c2be7f2989486cac8587a2a
beepbeepdaddad <emmetdoyle@ymail.com>
2015-04-12 15:37:32 +0100

    color changes added (bugs)

--- Assets/MoveBall.cs
+++ Assets/MoveBall.cs
@@ -5,6 +5,7 @@ public class MoveBall : MonoBehaviour {
 
 	private Rigidbody2D ballRigidbody;
 	private SpriteRenderer render;
+	public Transform ballTransform;
 
 	private float speed;
 	public float r, g, b;
@@ -15,6 +16,7 @@ public class MoveBall : MonoBehaviour {
 
 		ballRigidbody = this.rigidbody2D;
 		render = gameObject.GetComponent<SpriteRenderer> ();
+		ballTransform = this.transform;
 		r = 0f;
 		g = 0f;
 		b = 0f;
@@ -29,6 +31,22 @@ public class MoveBall : MonoBehaviour {
 	void Update () {
 		float move = Input.GetAxis ("Horizontal");
 
-		ballRigidbody.velocity = new Vector2 (move * speed, ballRigidbody.velocity.y);
+		ballRigidbody.velocity = new Vector3 (move * speed, ballRigidbody.velocity.y, 0);
+
+		Vector3 screenPos = GameObject.Find("MainCamera").GetComponent<Camera>().WorldToScreenPoint(ballTransform.position);
+		if (screenPos.x < GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section1) {
+			r = 1f;
+			g = 0f;
+			b = 0f;
+		} else if (screenPos.x > GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section1 && screenPos.x < GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section2) {
+			r = 1f;
+			g = 1f;
+			b = 0f;
+		} else {
+			r = 0f;
+			g = 0f;
+			b = 0f;
+		}
+		render.color = new Color (r, g, b, 1);
 	}
 }
