4c218d045c1a9892375e5e485342de29ba725006
beepbeepdaddad <emmetdoyle@ymail.com>
2015-04-12 20:42:53 +0100

    color changing working smootly

--- Assets/MoveBall.cs
+++ Assets/MoveBall.cs
@@ -17,9 +17,9 @@ public class MoveBall : MonoBehaviour {
 		ballRigidbody = this.rigidbody2D;
 		render = gameObject.GetComponent<SpriteRenderer> ();
 		ballTransform = this.transform;
-		r = 0f;
-		g = 0f;
-		b = 0f;
+		r = 0.0f;
+		g = 0.0f;
+		b = 0.0f;
 		render.color = new Color (r, g, b, 1);
 
 		speed = 5.0f;
@@ -36,17 +36,34 @@ public class MoveBall : MonoBehaviour {
 		Vector3 screenPos = GameObject.Find("MainCamera").GetComponent<Camera>().WorldToScreenPoint(ballTransform.position);
 		if (screenPos.x < GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section1) {
 			r = 1f;
-			g = 0f;
+			g = screenPos.x * (1f / GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section1);
+			print (g);
 			b = 0f;
 		} else if (screenPos.x > GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section1 && screenPos.x < GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section2) {
-			r = 1f;
+			r = (((GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section1) - screenPos.x) * (1f / (GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section1))) + 1;
 			g = 1f;
 			b = 0f;
-		} else {
+		} else if (screenPos.x > GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section2 && screenPos.x < GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section3){
 			r = 0f;
-			g = 0f;
-			b = 0f;
+			g = 1f;
+			b = (screenPos.x - GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section2) * (1f / GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section1);
 		}
+		else if (screenPos.x > GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section3 && screenPos.x < GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section4){
+			r = 0f;
+			g = (((GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section3) - screenPos.x) * (1f / (GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section1))) + 1;
+			b = 1f;
+		}
+		else if (screenPos.x > GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section4 && screenPos.x < GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section5){
+			r = (screenPos.x - GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section4) * (1f / GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section1);
+			g = 0f;
+			b = 1f;
+		}
+		else if (screenPos.x > GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section5 && screenPos.x < GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section6){
+			r = 1f;
+			g = 0f;
+			b = (((GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section5) - screenPos.x) * (1f / (GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().section1))) + 1;
+		}
+
 		render.color = new Color (r, g, b, 1);
 	}
 }
