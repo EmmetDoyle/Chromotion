b1a400e99a5914fe2784e74e3f0483ddb090c6eb
beepbeepdaddad <emmetdoyle@ymail.com>
2015-04-13 15:54:15 +0100

    started on blocks, positioned at the bottom

---
+++ Assets/MoveBlock.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBlock : MonoBehaviour {
+
+
+	public Transform blockTransform;
+	public bool printed;
+
+	// Use this for initialization
+	void Start () {
+		blockTransform = this.transform;
+
+		Vector3 screenPos = GameObject.Find("MainCamera").GetComponent<Camera>().WorldToScreenPoint(blockTransform.position);
+		screenPos.y = 0;
+
+		printed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!printed) {
+			print (GameObject.Find ("MainCamera").GetComponent<ScreenSize> ().bottomBorder.y);
+		}
+		blockTransform.position = new Vector3 (blockTransform.position.x, GameObject.Find("MainCamera").GetComponent<ScreenSize>().bottomBorder.y, blockTransform.position.z);
+	}
+}
--- Assets/ScreenSize.cs
+++ Assets/ScreenSize.cs
@@ -3,12 +3,28 @@ using System.Collections;
 
 public class ScreenSize : MonoBehaviour {
 
+	public Transform screenTransform;
+
 	public int screenWidth;
 	public int section1, section2, section3, section4, section5, section6;
+	public Vector3 bottomBorder;
 
+
+	public float dist;  
+	public float leftBorder;
+	public float rightBorder;
+
+	public bool printed;
 
 	// Use this for initialization
 	void Start () {
+
+
+		screenTransform = this.transform;
+
+		dist = (transform.position.y);
+		print ("dist is " + dist);
+
 		screenWidth = Screen.width;
 		section1 = screenWidth / 6;
 		section2 = (int)(screenWidth / 6) * 2;
@@ -24,11 +40,18 @@ public class ScreenSize : MonoBehaviour {
 		print ("section4 is " + section4);
 		print ("section5 is " + section5);
 		print ("section6 is " + section6);
+
+		printed = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		bottomBorder = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth / 2, 0, 0));
 
+		if (!printed) {
+			print ("bottom is " + bottomBorder.y);
+			printed = true;
+		}
 	
 	}
 }
